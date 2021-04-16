d7e85150cd7309be2f1b58fd40af4ce6065059cf
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-04-16 19:51:08 +0100

    passando coisas para o react

--- the-pomodoro/src/App.js
+++ the-pomodoro/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //return Header();
+  return SignUp();
+  //return Login();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +26,58 @@ function App() {
   );
 }
 
+function SignIn(){
+  return (
+    <div id="sign">
+			<form>
+				<div className="title">The Pomodoro</div>
+
+				<div className="label">Username</div>
+				<div className="input"><input type="text"></input></div>
+	
+				<div className="label">Password</div>
+				<div className="input"><input type="text"></input></div>
+	
+				<div className="link"><a>Ainda não se registou? Clique aqui</a></div>
+	
+				<div className="button"><input type="submit" value="Sign In"></input></div>
+			</form>
+		</div>
+  );
+}
+
+function SignUp(){
+  return (
+    <div id="sign">
+			<form>
+				<div className="title">The Pomodoro</div>
+
+				<div className="label">Username</div>
+				<div className="input"><input type="text"></input></div>
+	
+				<div className="label">Password</div>
+				<div className="input"><input type="text"></input></div>
+
+				<div className="label">Confirmação da password</div>
+				<div className="input"><input type="text"></input></div>
+	
+				<div className="link"><a>Ainda não se registou? Clique aqui</a></div>
+	
+				<div className="button"><input type="submit" value="Sign Up"></input></div>
+			</form>
+		</div>
+  );
+}
+
+function Header(){
+  return (
+    <div id="navbar">
+			<ul>
+				<li style={{float:"left"}}><a>The Pomodoro</a></li>
+				<li><a>Perfil</a></li>
+				<li><input type="text" value="Pesquisar"></input></li>
+			</ul>
+		</div>
+  );
+}
 export default App;
