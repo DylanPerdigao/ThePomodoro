30af5397cad055f274a2574bd5afb01f0e4f7023
Bruno Faria <brunofaria1322@gmail.com>
2021-04-16 23:56:58 +0100

    seguir a estrutura

--- the-pomodoro/src/App.js
+++ the-pomodoro/src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import { Switch, Route, useLocation, Link } from 'react-router-dom';
+import home from './pages/Home'
 import Header from './components/Header'
-import SignIn from './components/signIn/SignIn'
-import SignUp from './components/signIn/SignIn'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import './App.css';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-
   return (
     <>
       <Header/>
       <content>
         <Switch id="main">
-        {/*<Route path="/" component={home} /> */}
+        <Route path="/" component={home} />
         <Route path="/signin" component={SignIn}>
         </Route>
         <Route path="/signup" component={SignUp}>
@@ -50,71 +50,5 @@ function App() {
       </header>
     </div>
   );
-}
-
-function SignIn(){
-  return (
-    <div id="sign">
-			<form>
-				<div className="title">The Pomodoro</div>
-
-				<div className="label">Username</div>
-				<div className="input"><input type="text"></input></div>
-	
-				<div className="label">Password</div>
-				<div className="input"><input type="text"></input></div>
-	
-				<div className="link"><a>Ainda não se registou? Clique aqui</a></div>
-	
-				<div className="button"><input type="submit" value="Sign In"></input></div>
-			</form>
-		</div>
-  );
-}
-
-function SignUp(){
-  return (
-    <div id="sign">
-			<form>
-				<div className="title">The Pomodoro</div>
-
-				<div className="label">Username</div>
-				<div className="input"><input type="text"></input></div>
-	
-				<div className="label">Password</div>
-				<div className="input"><input type="text"></input></div>
-
-				<div className="label">Confirmação da password</div>
-				<div className="input"><input type="text"></input></div>
-	
-				<div className="link"><a>Ainda não se registou? Clique aqui</a></div>
-	
-				<div className="button"><input type="submit" value="Sign Up"></input></div>
-			</form>
-		</div>
-  );
-}
-
-function Header(){
-  return (
-    <div id="navbar">
-			<ul>
-				<li style={{float:"left"}}><a>The Pomodoro</a></li>
-				<li><a>Perfil</a></li>
-				<li><input type="text" value="Pesquisar"></input></li>
-			</ul>
-		</div>
-  );
-}
-
-function SearchRecipe(){
-  return (
-    <div id="search">
-    <form>
-      <div className="label">Procurar receita por ingredientes</div>
-      <div className="input"><input type="text" placeholder="Nome da receita ou dos ingredientes"></input></div>
-      </form>
-    </div>
-  )
 }
 */
---
+++ the-pomodoro/src/pages/Home.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return (
+      <div id="search">
+      <form>
+        <div className="label">Procurar receita por ingredientes</div>
+        <div className="input"><input type="text" placeholder="Nome da receita ou dos ingredientes"></input></div>
+        </form>
+      </div>
+    );
+};
