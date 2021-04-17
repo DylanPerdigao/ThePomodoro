5ee7c590c52c641bd85b9fc3308a426fb661b570
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-04-17 12:02:05 +0100

    Update Profile.js

--- the-pomodoro/src/pages/Profile.js
+++ the-pomodoro/src/pages/Profile.js
@@ -1,28 +1,27 @@
+import Post from './../components/Post'
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
     return (
         <div id="profile">
             <div id="inner">
-                <h1>NOME DA RECEITA</h1>
-                <h3>By AUTOR</h3>
-                <div class="recipeBox">
-                    <div class="horizontal">
-                        <div>
-                            <h2>Descrição</h2>
-                            <p>Lorem Ipsum Dolor</p>
-                        </div>
-                        <div>
-                            <img src="favicon.ico"></img>
-                        </div>
-                    </div>
-                    <h2>Ingredientes</h2>
-                    <ul>
-                        <ol>Lorem Ipsum Dolor</ol>
-                        <ol>Lorem Ipsum Dolor</ol>
-                    </ul>
-                    <h2>Preparação</h2>
-                    <p>Lorem Ipsum Dolor</p>
+                <h1>NOME</h1>
+
+                <button>Follow</button>
+                <button>Unfollow</button>
+                
+
+                <h2>Minhas Receitas</h2>
+                <div className="myRecipes">
+                    <Post />
                 </div>
+                
+
+                <h2>Minhas Receitas</h2>
+                <div className="favRecipes">
+                    <Post />
+                </div>
+
             </div>
         </div>
     );
