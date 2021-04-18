8a7f8b1ce9e98105af4e483f080ad81f6bb48516
Bruno Faria <brunofaria1322@gmail.com>
2021-04-18 01:58:44 +0100

    signin done
    token guardado no localstorage

--- the-pomodoro/src/pages/SignIn.js
+++ the-pomodoro/src/pages/SignIn.js
@@ -1,28 +1,41 @@
 import '../styles/Sign.css';
+let base64 = require('base-64');
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
-    const signin = () => {
-        
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Basic dGVzdGU6dGVzdGU=");
-        myHeaders.append("Access-Control-Allow-Origin", "http://localhost:3000/");
-        myHeaders.append("Access-Control-Allow-Methods", "GET");
-        //myHeaders.append("Access-Control-Allow-Headers", "Basic dGVzdGU6dGVzdGU=");
-        
+    const state = {
+        username: "",
+        password: ""
+    }
 
-        fetch('https://thepomodoro.herokuapp.com/login', {
+    const setUsername = (event) => {
+        state.username = event.target.value;
+    }
+    const setPassword = (event) => {
+        state.password = event.target.value;
+    }
+
+    const signin = () => {
+
+        fetch('http://127.0.0.1:5000/login', {
             method: 'GET',
-            headers: myHeaders,
+            headers: {
+                "Authorization" : "Basic "+base64.encode(`${state.username}:${state.password}`),
+            },
             redirect: 'follow',
+            
         })
-        .then(response => response)
-        .then(result => console.log(result))
+        .then(response => response.json())
+        .then(result => {
+            localStorage.setItem("token", result.token);
+        })
         .catch(error => {
             alert('Unable to reach API.');
             console.log(error);
         });
+
+        //console.log(localStorage.getItem("token"))
     }
 
     return (
@@ -32,10 +45,10 @@ export default () => {
 
                 <label>Username</label>
                 
-                <input type="text"></input>
+                <input type="text" onChange = {setUsername}></input>
 
                 <label>Password</label>
-                <input type="text"></input>
+                <input type="text" onChange = {setPassword}></input>
 
                 <a href = "/signup">Ainda não se registou? Clique aqui</a>
 
