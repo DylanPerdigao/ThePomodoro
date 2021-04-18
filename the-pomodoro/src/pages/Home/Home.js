413ab73795f680017aaca18978281a42205e724c
Bruno Faria <brunofaria1322@gmail.com>
2021-04-18 15:43:08 +0100

    search by ingredients

--- the-pomodoro/src/pages/Home/Home.js
+++ the-pomodoro/src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { encode } from 'base-64';
 import { useState, useEffect } from 'react';
 import Post from '../../components/Post'
 import './Home.css';
@@ -5,15 +6,69 @@ import './Home.css';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
     const [state, setState] = useState ({
-        search: undefined,
+        search: null,
         loading: true,
         error: false,
-        popularRecipesComponents: null
+        RecipesComponents: null
     });
     function setSearch(e){state.search = e.target.value}
+
     function search(e){
-      if(e.keyCode == 13){
+      if(e.keyCode === 13){
+        let a = ""; 
+        a = document.getElementById("mySearch").value;
+
         state.search=document.getElementById("mySearch").value;
+        let list = a.split(',').map(ing =>(ing.trim()));
+        //console.log(list)
+
+        fetch('https://thepomodoro.herokuapp.com/search_by_ingredients', {
+            method: 'POST',
+            redirect: 'follow',
+            body: JSON.stringify({"ingredients": list}),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then(response => response.json())
+        .then(resp => {
+            console.log(resp);
+            
+            const recipes = resp["recipes"];
+
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            let popularRecipesComponents = recipes.map( p => (
+                <Post 
+                    id={p.id}
+                    name={p.name}
+                    description={p.description}
+                    image={p.image}
+                    rating= {p.rating}
+
+                    author={p.author}
+
+                    onClick = { () => {window.location.href=`/recipe/${p.id}`;}}
+                />
+            ));
+            
+            setState({
+                loading: false,
+                error: false,
+                name: resp.name,
+                RecipesComponents: popularRecipesComponents
+            })
+
+        })
+        .catch(error => {
+            console.log(error);
+            setState({
+                loading: false,
+                error: true,
+                name: ""
+            })
+        });
+
+
         console.log(state.search);
       }
     }
@@ -54,7 +109,7 @@ export default () => {
                     loading: false,
                     error: false,
                     name: resp.name,
-                    popularRecipesComponents: popularRecipesComponents
+                    RecipesComponents: popularRecipesComponents
                 })
 
             })
@@ -73,15 +128,15 @@ export default () => {
         <div id="home">
         <div id="searchBar">
             <label>Procurar receita por ingredientes</label>
-            <input id="mySearch" type="text" placeholder="Nome da receita ou dos ingredientes" value={state.search} onClick={setSearch} onKeyUp={search}></input>
+            <input id="mySearch" type="text" placeholder="ing1, ing2, ..." value={state.search} onClick={setSearch} onKeyUp={search}></input>
         </div>
         <div id="feed">
-            <label>Receitas Populares</label>
+            {state.search === true ? (<label>Receitas Encontradas</label>) : (<label>Receitas Populares</label>)}
             {state.loading ? (<p>loading...</p>
             ) : state.error ? (<h1>AN ERROR HAS OCURRED!</h1>
             ) : (
                 <div className="Recipes">
-                    {state.popularRecipesComponents}
+                    {state.RecipesComponents}
                 </div>
             )}
         </div>
